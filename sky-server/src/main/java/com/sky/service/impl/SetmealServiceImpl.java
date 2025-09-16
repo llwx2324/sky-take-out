09e90702dac8798dec3ccd179342bca381ffb3a2
81287 <81287317@qq.com>
2025-09-16 16:19:28 +0800

    套餐分页查询代码

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,12 +1,17 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,13 +35,22 @@ public class SetmealServiceImpl implements SetmealService {
         //属性拷贝
         BeanUtils.copyProperties(setmealDTO, setmeal);
         setmealMapper.insert(setmeal);
-
+        Long id = setmeal.getId();//主键返回
         //新增套餐菜品关联关系
         List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
         if (setmealDishes != null && setmealDishes.size() > 0) {
+            setmealDishes.forEach(item -> {
+                item.setSetmealId(id);
+            });
             setmealDishMapper.insertBatch(setmealDishes);
         }
 
+    }
 
+    @Override
+    public PageResult page(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
+        Page<SetmealVO> list = setmealMapper.pageQuery(setmealPageQueryDTO);
+        return new PageResult(list.getTotal(), list.getResult());
     }
 }
